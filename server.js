bf0d834ce10d0bd25cde734628f42383b476546f
nikhita-code <nikhita.kalamkar@gmail.com>
2025-09-18 02:42:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Dockerized Node.js app 🚀");
+  res.send("Hello from Node.js app deploying using AWS ECS Farget Cluster!! Hello , Nikhita here:) ");
 });
 
 app.listen(PORT, () => {
